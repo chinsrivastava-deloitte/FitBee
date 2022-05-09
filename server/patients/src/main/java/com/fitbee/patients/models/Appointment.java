d368b6d7933539f00c1581a4c45978fa6dc507f4
chinsrivastava-deloitte <chinsrivastava@deloitte.com>
2022-05-09 17:42:34 +0530

    mappings

--- server/patients/src/main/java/com/fitbee/patients/models/Appointment.java
+++ server/patients/src/main/java/com/fitbee/patients/models/Appointment.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
+@Table
 public class Appointment {
 
     @Id
--- server/patients/src/main/java/com/fitbee/patients/models/Doctor.java
+++ server/patients/src/main/java/com/fitbee/patients/models/Doctor.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@Table
 public class Doctor {
 
     @Id
